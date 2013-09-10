b93fc77632e07944cf80b751153f7fbe56622676
Ørjan Markhus Lillevik <olillevik@gmail.com>
2013-09-10 20:08:01 +0200

    Adding support for standalone mode. Adding jdk to ensure workshop is runnable without java preinstalled

--- runFitNesseDirectly.bat
+++ runFitNesseDirectly.bat
@@ -1,2 +1,9 @@
-java -jar fitnesse.jar -o -p 8980 -e 0
+SET JAVA_HOME=jdk1.7
+SET Path=jdk1.7\bin;%Path%
+
+SET CP=fitlibrary-2.0.jar
+SET java.class.path=%CP%
+
+
+java -cp %CP% -jar fitnesse-standalone.jar -o
 pause
