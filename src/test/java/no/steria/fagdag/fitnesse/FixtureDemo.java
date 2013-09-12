d085ea7c3c7ad072d8183bb357a063a8cafb5490
Øystein Urdahl Hardeng <oystein.urdahl@gmail.com>
2013-09-12 20:57:38 +0200

    Elaborate on DoFixture

--- src/test/java/no/steria/fagdag/fitnesse/FixtureDemo.java
+++ src/test/java/no/steria/fagdag/fitnesse/FixtureDemo.java
@@ -13,33 +13,59 @@ import fitlibrary.suite.SuiteFixture;
 
 public class FixtureDemo extends SuiteFixture {
 	private List<ListData> list = new ArrayList<>();
-	
-	public ColumnFixture subtraksjon() { 
+
+	public ColumnFixture subtraksjon() {
 		return new SubtraksjonColumnFixture();
 	}
-	
+
 	public SetUpFixture setUpColorLinker() {
 		return new SetUpColorLinker();
 	}
-	
+
 	public RowEntryFixture rowEntryColorLinker() {
 		return new RowEntryColorLinker();
 	}
-	
-	public SetUpFixture testDataForValideringAvLister() { 
+
+	public SetUpFixture testDataForValideringAvLister() {
 		list = new ArrayList<>();
 		return new SetUpListData(list);
 	}
-	
-	public Set<ListData> demonstrasjonAvSetFixture() { 
+
+	public Set<ListData> demonstrasjonAvSetFixture() {
 		return new HashSet<ListData>(list);
 	}
-	
-	public List<ListData> demonstrasjonAvArrayFixture() { 
+
+	public List<ListData> demonstrasjonAvArrayFixture() {
 		return list;
 	}
-	
+
 	public SubsetFixture demonstrasjonAvSubSetFixture() {
 		return new SubsetFixture(list);
+	}
+
+	public String denneMetodenSetterSammenEtMedEtOrdDotShowViserReturverdienEtterDenSisteCellen(
+			String s, String s2) {
+		return s + " " + s2;
+	}
+
+	public String denneMetodenSetterSammenEtMedEtOrdDotCheckValidererReturverdienMotDenSisteCelleIRaden(
+			String s1, String s2) {
+		return s1 + " " + s2;
+	}
+
+	public void hvisDenNaturligeSetningsbygningenTilsierAtDetKommerEtterHverandreMuE5ManHaEnTomCelleSomHer(
+			String arg1, String arg2) {
+	}
+
+	public boolean getEnMetodeSomReturnerTrueVilFargesGruF8nn() {
+		return true;
+	}
+
+	public boolean getMensEnMetodeSomReturnerFalseVilFargesRuF8d() {
+		return false;
+	}
+
+	public boolean negererSlikAtDenneSomReturnererFalseBlirGruF8nn() {
+		return false;
 	}
 }
